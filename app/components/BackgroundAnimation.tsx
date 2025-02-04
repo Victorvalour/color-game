ac3d747d906ab8d09435ec31c5d1752aa6ecbecf
victorvalour <ewehvictor7@gmail.com>
2025-02-04 17:47:49 +0100

    Fixed Type error

--- app/components/BackgroundAnimation.tsx
+++ app/components/BackgroundAnimation.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, JSX } from "react"
 import styles from "./BackgroundAnimation.module.css"
 
 type AnimationType = "lasers" | "rain" | "shapes"
